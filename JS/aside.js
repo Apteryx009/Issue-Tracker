ba4995c20b1457d24ba0d871af97eeb1cef4f725
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-07 01:25:39 -0500

    sidebar now working with user action

--- JS/aside.js
+++ JS/aside.js
@@ -4,17 +4,16 @@
 let asideButtons = document.querySelectorAll('.aside_list-item');
 console.log(asideButtons);
 
-asideButtons[0].addEventListener('click', toPage('userHome.html'))
-// asideButtons.addEventListener('click')
-// asideButtons.addEventListener('click')
-// asideButtons.addEventListener('click')
-// asideButtons.addEventListener('click')
+var nameObt = ['index.html', 'userHome.html', ]
 
-
-//Function to take user to html page when they
-//click on a certain button
-function toPage(pageName)
-{
-    
-    window.location.href = `../HTML/${pageName}`;
+//Note: If user is already on say userHome.html, then u won't see a different page when you click
+//Dashbaord Home because it's already on that page. It will just refresh. 
+for(let i =0;  i < asideButtons.length; i++){
+ console.log(i);
+ asideButtons[i].addEventListener('click', e=> {
+    console.log(nameObt[i])
+    window.location.href = `../HTML/${nameObt[i]}`;
+ });
 }
+
+console.log(nameObt[0]);
