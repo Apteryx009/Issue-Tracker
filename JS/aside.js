dfa4d07cbf4f1e5c72fefee17d00298d7ede3b86
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-07 01:05:45 -0500

    Fix page refreshing in infinite loop

--- JS/aside.js
+++ JS/aside.js
@@ -1,1 +1,20 @@
 //Used for sidebar
+
+//Array of button on sidebar
+let asideButtons = document.querySelectorAll('.aside_list-item');
+console.log(asideButtons);
+
+asideButtons[0].addEventListener('click', toPage('userHome.html'))
+// asideButtons.addEventListener('click')
+// asideButtons.addEventListener('click')
+// asideButtons.addEventListener('click')
+// asideButtons.addEventListener('click')
+
+
+//Function to take user to html page when they
+//click on a certain button
+function toPage(pageName)
+{
+    
+    window.location.href = `../HTML/${pageName}`;
+}
