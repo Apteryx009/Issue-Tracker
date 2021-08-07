8aca6c448d01c36f76f7a5a84f51c5cebdae7b0a
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-07 00:24:50 -0500

    restructured UI layout

---
+++ JS/indexScript.js
@@ -0,0 +1,8 @@
+let userAction = document.querySelector('#DashHome')
+console.log(userAction);
+
+userAction.addEventListener('click', loadPage)
+
+function loadPage(e){
+    window.location.href = "../HTML/userHome.html";
+}
