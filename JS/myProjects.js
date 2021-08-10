c1ebbba379693f697e6c0ce0df8c37a08cfcebc8
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-09 23:37:56 -0500

    Added create tickets interface

--- JS/myProjects.js
+++ JS/myProjects.js
@@ -1,10 +1,17 @@
 //A test button which will load projectTickets.html at user request
 let userAction = document.querySelector('#loadProjectTickets')
-console.log(userAction);
+let userCreateTicket = document.querySelector('#createNewTicket')
+
 
 userAction.addEventListener('click', loadPage)
+userCreateTicket.addEventListener('click', loadPage2)
 
 function loadPage(e){
     console.log('ello chap');
     window.location.href = "../HTML/projectTickets.html";
+}
+
+function loadPage2(e){
+    console.log('ello chap2');
+    window.location.href = "../HTML/createTicket.html";
 }
