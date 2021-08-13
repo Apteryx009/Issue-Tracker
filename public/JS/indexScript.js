9839c6c58ab78934517e6c1c484def8bbcca638f
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-13 00:11:11 -0500

    Completed Several Firebase Config settings

--- public/JS/indexScript.js
+++ public/JS/indexScript.js
@@ -1,9 +1,13 @@
-//Used to greet user
+
 let userAction = document.querySelector('#DashHome')
 console.log(userAction);
 
 userAction.addEventListener('click', loadPage)
 
 function loadPage(e){
-    window.location.href = "../HTML/userHome.html";
+  window.location.href = "../HTML/userHome.html";
+
+    // const app = firebase.app();
+    // console.log(app)
 }
+
