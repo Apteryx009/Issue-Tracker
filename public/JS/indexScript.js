44a18f422d66da90702e9785e7f6ac40d3d8ad05
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-14 01:04:19 -0500

    Config for Firebase

--- public/JS/indexScript.js
+++ public/JS/indexScript.js
@@ -1,3 +1,15 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+// import firebase from "../../node_modules/firebase";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+// import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+// import "firebase/auth";
+// import "firebase/firestore";
+// import "firebase/database";
 
 let userAction = document.querySelector('#DashHome')
 console.log(userAction);
@@ -7,7 +19,11 @@ userAction.addEventListener('click', loadPage)
 function loadPage(e){
   window.location.href = "../HTML/userHome.html";
 
-    // const app = firebase.app();
-    // console.log(app)
+     
+     console.log(app)
 }
 
+function emailAndPasscode(){
+   const auth = firebase.auth();
+   auth.signInWithEmailAndPassword(email, pass);
+}
