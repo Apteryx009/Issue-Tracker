5cf7d3d959d00f28e3df333692b0ed0c4dcb2d1a
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-15 01:36:30 -0500

    Changing Firebase Files

--- public/JS/indexScript.js
+++ public/JS/indexScript.js
@@ -1,29 +1,93 @@
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-// import firebase from "../../node_modules/firebase";
-// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
-// import * as firebase from "firebase/app"
+///// User Authentication /////
 
-// If you enabled Analytics in your project, add the Firebase SDK for Analytics
-// import "firebase/analytics";
+const auth = firebase.auth();
 
-// Add the Firebase products that you want to use
-// import "firebase/auth";
-// import "firebase/firestore";
-// import "firebase/database";
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
 
-let userAction = document.querySelector('#DashHome')
-console.log(userAction);
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
 
-userAction.addEventListener('click', loadPage)
+const userDetails = document.getElementById('userDetails');
 
-function loadPage(e){
-  window.location.href = "../HTML/userHome.html";
 
-     
-     console.log(app)
-}
+const provider = new firebase.auth.GoogleAuthProvider();
 
-function emailAndPasscode(){
-   const auth = firebase.auth();
-   auth.signInWithEmailAndPassword(email, pass);
-}
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        // signed in
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`;
+    } else {
+        // not signed in
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = '';
+    }
+});
+
+
+
+///// Firestore /////
+
+const db = firebase.firestore();
+
+const createThing = document.getElementById('createThing');
+const thingsList = document.getElementById('thingsList');
+
+
+let thingsRef;
+let unsubscribe;
+
+auth.onAuthStateChanged(user => {
+
+    if (user) {
+
+        // Database Reference
+        thingsRef = db.collection('things')
+
+        createThing.onclick = () => {
+
+            const { serverTimestamp } = firebase.firestore.FieldValue;
+
+            thingsRef.add({
+                uid: user.uid,
+                name: faker.commerce.productName(),
+                createdAt: serverTimestamp()
+            });
+        }
+
+
+        // Query
+        unsubscribe = thingsRef
+            .where('uid', '==', user.uid)
+            .orderBy('createdAt') // Requires a query
+            .onSnapshot(querySnapshot => {
+                
+                // Map results to an array of li elements
+
+                const items = querySnapshot.docs.map(doc => {
+
+                    return `<li>${doc.data().name}</li>`
+
+                });
+
+                thingsList.innerHTML = items.join('');
+
+            });
+
+
+
+    } else {
+        // Unsubscribe when the user signs out
+        unsubscribe && unsubscribe();
+    }
+});
+
