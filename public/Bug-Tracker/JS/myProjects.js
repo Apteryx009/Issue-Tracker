4a8b7fd68627328901413cb0e9eb2a89bdee4f44
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-20 02:32:18 -0500

    Improved createTicket page

--- public/Bug-Tracker/JS/myProjects.js
+++ public/Bug-Tracker/JS/myProjects.js
@@ -24,21 +24,26 @@ function loadPage2(e){
 const db = firebase.firestore();
 const userUID = localStorage.getItem('userUID');
 
-db.collection("users")
-    .onSnapshot(function(querySnapshot) {
-        let users = [];
-        querySnapshot.forEach(function(doc) {
-            users.push(doc.data());
-        }); 
-       // console.log(users);     
-    });
+// db.collection("users")
+//     .onSnapshot(function(querySnapshot) {
+//         let users = [];
+//         querySnapshot.forEach(function(doc) {
+//             users.push(doc.data());
+//         }); 
+//        // console.log(users);     
+//     });
 
     var data = db.doc('/users/rk8B6Z18JghZWybTt13Ubm0cbn82/projects/projectDetails');
 
+
+    let ourData = document.getElementById('testData')
+    
+//testing
     data.get().then((doc) => {
         if (doc.exists) {
             var certainField = doc.data();
             console.log("Document data:", certainField.name);
+            ourData.innerText = certainField.name;
         } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
@@ -49,13 +54,31 @@ db.collection("users")
 
 
 
-// db.collection('users').doc(userUID).get().then((doc) =>{
-//     const html =
-//     `
-//         <div>Your data is: ${doc.data().bio} </div>
-//     `;
+// //dynamically Create and list projects that user has access to
+// var listSub1 = document.querySelector('.listSub1');
+// var projectName = document.createElement('a');
+// projectName.setAttribute('class', 'list-group-item list-group-item-action list-group-item-danger');
+// projectName.setAttribute("href", "https://www.reddit.com/");
+// projectName.appendChild(document.createTextNode("data"));
+// listSub1.appendChild(projectName);
 
-//     alert(doc.data().bio);
-//     testData.innerHTML = html;
-// })
 
+// var data = db.doc(`/users/rk8B6Z18JghZWybTt13Ubm0cbn82/projects/projectDetails`);
+// let size; //Amounts of projects a given user has
+// db.doc("/users/rk8B6Z18JghZWybTt13Ubm0cbn82/projects").get().then(function(querySnapshot) {      
+//     size = (querySnapshot.size); 
+// });
+
+// //TODO: To get element, I need it's ID, but to get ID, i need to save it when I create it. 
+// //Thus, I need to focus on creating tickets for projectd before recieving it.
+
+// function getProjectData(){
+
+//     //For listSub1
+//     for(let i =0; i < size; i++){
+//       console.log(size)
+//     }
+
+// }
+
+// getProjectData();
