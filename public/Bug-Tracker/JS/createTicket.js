cda282030a3235625b48036d3d5da3981a283f7a
Jacob Mccalip <jacobmccalip@gmail.com>
2021-08-23 01:27:16 -0500

    Added functionality to display names of all users

--- public/Bug-Tracker/JS/createTicket.js
+++ public/Bug-Tracker/JS/createTicket.js
@@ -5,45 +5,47 @@ document.getElementById('date').innerHTML = date;
 toggle between hiding and showing the dropdown content */
 function assignee() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
+}
+
+function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     div = document.getElementById("myDropdown");
     a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
     }
-  } 
+}
 
-  const db = firebase.firestore();
-  
-  //Get names of all uses and append (create new <a> tags) into the dropdown we created 
+const db = firebase.firestore();
+
+//Get names of all uses and append (create new <a> tags) into the dropdown we created 
 // retrieve a collection
 
 const userList = document.querySelector('#user-list');
 
-db.collection('users').get().then(function (querySnapshot)  {
-    querySnapshot.docs.forEach(function(doc) {
+//Filter through all user docs
+db.collection('users').get().then(function (querySnapshot) {
+    querySnapshot.docs.forEach(function (doc) {
         renderDoc(doc)
     });
 });
 
-function renderDoc(doc){
-    let li = document.createElement('li');
-    let name = document.createElement('span');
-    
-    li.setAttribute('data-id', doc.id);
-    console.log(doc.data().name)
-    name.textContent = doc.data().name;
+function renderDoc(doc) {
+    let a = document.createElement('a');
+    //In case we need to ref it later  
+    a.setAttribute('data-id', doc.id);
 
-    li.appendChild(name);
+    //Fill drop down menu with name of users
+    a.textContent = doc.data().name;
 
+    //Update UI
+    const dropDownList = document.querySelector('#myDropdown');
+    dropDownList.appendChild(a);
 }
